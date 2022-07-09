6c568e6dfd1c38b9b424f626db2e69f43cb5129c
Matt Krueger <mkrueger@rstms.net>
2022-07-09 11:15:35 -0500

    add module make var; change project to module

--- make.include/common.mk
+++ make.include/common.mk
@@ -1,9 +1,10 @@
 # common - initialization, variables, functions
 
-project = vmware_workstation_cli
+project = vmware-workstation-cli
+module = vmware_workstation_cli
 organization = rstms
 branch != git branch | awk '/\*/{print $$2}'
-version != awk <$(project)/version.py -F\" '/^__version__/{print $$2}'
+version != awk <$(module)/version.py -F\" '/^__version__/{print $$2}'
 python_src != find . -name \*.py
 other_src := $(call makefiles) LICENSE README.md
 src := $(python_src) $(other_src)
