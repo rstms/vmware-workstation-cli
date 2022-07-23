943da4deff2c3294bc5c8b49089faa0f14430209
Matt Krueger <mkrueger@rstms.net>
2022-07-23 14:39:37 -0500

    Bump version: 0.1.3 → 0.1.4

--- vmware_workstation_cli/version.py
+++ vmware_workstation_cli/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-07-23 14:38:28-05:00"
+__timestamp__ = "2022-07-23 14:39:37-05:00"
